82593edd148098be08a1b39730e0f8cd3f075afb
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-07-18 16:15:16 +0530

    Quick Notes initialized

---
+++ src/preload/index.ts
@@ -0,0 +1,13 @@
+import { contextBridge } from 'electron'
+
+if (!process.contextIsolated) {
+  throw new Error('contextIsolation must be enabled in the BrowserWindow')
+}
+
+try {
+  contextBridge.exposeInMainWorld('context', {
+    //Todo
+  })
+} catch (error) {
+  console.error('Error : ', error)
+}
